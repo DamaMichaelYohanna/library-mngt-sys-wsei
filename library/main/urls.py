f9f820507461a359751a857387169f01c3d0dd43
DamaMichaelYohanna <get2dama11@gmail.com>
2024-12-12 11:16:16 +0100

    book management

--- library/main/urls.py
+++ library/main/urls.py
@@ -7,6 +7,9 @@ from . import views
 
 urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/update/<int:pk>/', views.update_book, name='update_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
     path('search_books/', views.search_books, name='search_books'),
    
 ]
--- library/main/views.py
+++ library/main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .forms import BookForm
 from .models import Author, Book, Publisher
 
 
@@ -41,3 +44,49 @@ def search_books(request):
     query = request.GET.get('query')
     books = Book.objects.filter(title__icontains=query)
     return render(request, 'search_books.html', {'books': books})
+
+
+def book_list(request):
+    """list book in admin site"""
+    # Get the search query from the request
+    search_query = request.GET.get('search', '')
+
+    # Filter books based on the search query
+    if search_query:
+        books = Book.objects.filter(
+            name__icontains=search_query  # Search by book name
+        )
+    else:
+        books = Book.objects.all()
+
+    # Render the HTML template with the list of books
+    return render(request, 'main/book_list.html', {'books': books, 'search_query': search_query})
+
+@login_required
+def update_book(request, pk):
+    """View to update book from admin site"""
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        # Populate the form with data from the POST request
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()  # Save the updated book to the database
+            return redirect('/books')  # Redirect to the book list page
+    else:
+        # Create a form pre-filled with the book's current data
+        form = BookForm(instance=book)
+
+    return render(request, 'main/update_book.html', {'form': form, 'book': book})
+
+
+@login_required
+def delete_book(request, pk):
+    """View to delete book by admin"""
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        book.delete()  # Delete the book from the database
+        return redirect('/books')  # Redirect to the book list page
+
+    return render(request, 'main/delete_book.html', {'book': book})
